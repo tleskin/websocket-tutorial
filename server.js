5df83a9c47ffb13be56a16170a1e689e497eae92
Thomas Leskin <tleskin@gmail.com>
2015-08-05 09:20:49 -0600

    Setup initial server.js

---
+++ server.js
@@ -0,0 +1,19 @@
+const http = require('http');
+const express = require ('express');
+
+const app = express();
+
+app.use(express.static('public'));
+
+app.get('/', function (req, res) {
+  res.sendFile(_dirname + '/public/index.html');
+});
+
+var port = process.env.PORT || 3000;
+
+var server = http.createServer(app)
+                 .listen(port, function () {
+                    console.log('Listening on port ' + port + '.');
+                  });
+
+module.exports = server;
