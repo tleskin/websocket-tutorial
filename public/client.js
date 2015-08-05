3abecb51a649434d466fe5ab91c6f54fce5140b6
Thomas Leskin <tleskin@gmail.com>
2015-08-05 10:41:09 -0600

    Votes can be cast and are tallied in the browser console across all users

--- public/client.js
+++ public/client.js
@@ -11,3 +11,15 @@ var statusMessage = document.getElementById('status-message');
 socket.on('statusMessage', function (message) {
   statusMessage.innerText = message;
 });
+
+var buttons = document.querySelectorAll('#choices button');
+
+for (var i = 0; i < buttons.length; i++) {
+  buttons[i].addEventListener('click', function () {
+    socket.send('voteCast', this.innerText);
+  });
+}
+
+socket.on('voteCount', function (votes) {
+  console.log(votes);
+});
--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const http = require('http');
 const express = require ('express');
-
 const app = express();
 
 app.use(express.static('public'));
@@ -20,14 +19,40 @@ module.exports = server;
 
 const io = require('socket.io')(server);
 
+var votes = {};
+
+function countVotes(votes) {
+  var voteCount = {
+    A: 0,
+    B: 0,
+    C: 0,
+    D: 0
+  };
+  for (vote in votes) {
+    voteCount[votes[vote]]++
+  }
+  return voteCount;
+}
+
 io.on('connection', function (socket) {
   console.log('A user has connected.', io.engine.clientsCount);
 
-  io.sockets.emit('usersConnected', io.engine.clientsCount);
+  io.sockets.emit('userConnection', io.engine.clientsCount);
 
   socket.emit('statusMessage', 'You have connected.');
 
+  socket.on('message', function (channel, message) {
+    if (channel === 'voteCast') {
+      votes[socket.id] = message;
+      console.log(votes);
+      socket.emit('voteCount', countVotes(votes));
+    }
+  });
+
   socket.on('disconnect', function () {
     console.log('A user has disconnected.', io.engine.clientsCount);
+    delete votes[socket.id];
+    socket.emit('voteCount', countVotes(votes));
+    io.sockets.emit('userConnection', io.engine.clientsCount);
   });
 });
