bbf866a568cf097854d99be6c52d82fb24efa349
Thomas Leskin <tleskin@gmail.com>
2015-08-05 09:44:17 -0600

    Can send message to a particular client

--- public/client.js
+++ public/client.js
@@ -5,3 +5,9 @@ var connectionCount = document.getElementById('connection-count');
 socket.on('usersConnected', function (count) {
   connectionCount.innerText = 'Connected Users: ' + count;
 });
+
+var statusMessage = document.getElementById('status-message');
+
+socket.on('statusMessage', function (message) {
+  statusMessage.innerText = message;
+});
--- server.js
+++ server.js
@@ -25,6 +25,8 @@ io.on('connection', function (socket) {
 
   io.sockets.emit('usersConnected', io.engine.clientsCount);
 
+  socket.emit('statusMessage', 'You have connected.');
+
   socket.on('disconnect', function () {
     console.log('A user has disconnected.', io.engine.clientsCount);
   });
